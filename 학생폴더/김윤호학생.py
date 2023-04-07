577ab2f23476f77429c4289e5110d2566a07f902
sangwoo <tkddn4508@gmail.com>
2023-04-07 11:30:22 +0900

    학생과제
    학생과제 - class4 과제들

---
+++ 학생폴더/김윤호학생.py
@@ -0,0 +1,76 @@
+number = {'66997744': 100000,'774466':20000}
+print(number["66997744"])
+class bank():   #입금,출금,잔액
+    def __init__(self,n):
+        self.deposit = 0 
+        self.withdraw = 0
+        self.number = n
+        #self.balance = bank.bal()   #값이 초기화 되면 안된다
+
+
+    def dep(self,de):   #입금
+        if de > 0:
+            self.deposit = de
+            n = input("계좌번호 입력")
+            if(n in self.number):
+                self.number[n] += self.deposit
+            else:
+                print("계좌 번호가 없네요")
+                return "TT"
+            #self.balance = self.balance + self.deposit
+            #bank.bal()
+            print(f"{self.deposit}원이 입금되었습니다")
+            print(f"통장 잔액{self.number[n]}원")
+
+    def wit(self,withd):    #출금
+        if withd > 0:
+            self.withdraw = withd
+            n = input("계좌번호 입력")
+            if(n in self.number):
+                if(self.number[n]-self.withdraw > 0 ):
+                    self.number[n] -= self.withdraw
+                    print(f"성공하였습니다 남은잔액{self.number[n]}")
+                else:
+                    print(f"잔액이 부족해 출금이 불가해요 남은 잔액{self.number[n]}")
+            else:
+                print("계좌 번호가 없네요")
+                return "TT"
+
+        
+        #self.balance = self.balance - self.withdraw
+        #if(self.balance >= 0):
+        #    print("잔액이 부족해요")
+        #else:
+        #    print(f"{self.withdraw}원 출금되었습니다.")
+        
+    def bal(self):      #잔액확인
+        #global balance
+        n = input("확인하고 싶은 계좌번호 입력")
+        if(n in self.number):
+            print(f"{self.number[n]}원")
+        else:
+            print("계좌 번호가 없네요")
+            return "TT"
+        balance = self.number[n]
+        
+        #print(f"현재 잔액은{balance}입니다")
+        return balance
+run = bank(number)
+while(True):
+    a = input('어떤 기능을 사용하실 건가요? 입금(d,D) 출금(w,W) 잔액확인(b,B) 종료는(t)')
+
+    if(a == 'd' or  a == 'D'):
+        try:
+            value = int(input("입금할 금액입력"))
+            if value >= 0 and value <= 9: 
+                run.dep(value)
+            else:
+                pass    
+        except:
+            pass
+    elif(a == 'w' or  a == 'W'):
+        run.wit(int(input("출금할 금액입력")))
+    elif(a == 'b' or  a == 'B'):
+        run.bal()
+    if(a == 't'):
+        break
---
+++ 학생폴더/임준_은행과제.py
@@ -0,0 +1,150 @@
+#  은행계좌 
+#  class 사용
+#  입금, 출금, 잔액조회 기능
+
+#balance = 0
+class Bank():
+    brr = None
+    #global balance 
+    
+
+    def __init__(self):
+        self.balance = 0
+
+    def account(self):
+        try:
+            self.id = str(input("아이디: "))
+            pw = str(input("비밀번호: "))
+        except:
+            pass
+        if self.id == 'junlim' and pw == '1234':
+            print("로그인 성공!")
+            print("==========================")
+            print("")
+            print("")
+            
+            Bank.order()
+        else:
+            print("")
+            print("")
+            print("계정 불일치")
+            err = input("다시 입력: r / 종료: q  --> ")
+            if err >='a' and err <='z'or err >='A' and err <='Z':  
+                if err == 'r' or err == 'R':
+                    Bank.account()
+
+                elif err == 'q' or err == 'Q':
+                    print("종료합니다.")
+
+                else:
+                    pass
+                
+            
+
+    def order(self): # 입출금 명령
+
+        print(self.id, "님의 계좌:")
+        print("")
+        print("")
+        Bank.balance_inquiry()
+        print("")
+        while True:
+            brr = input("입금: d / 출금: w / 송금: s / 종료: q  --> ")
+            if brr == 'd' or brr == 'D':
+                Bank.desposit()
+                print("")
+                Bank.balance_inquiry()
+                
+            elif brr == 'w' or brr == 'W':
+                Bank.withdraw()
+                print("")
+                Bank.balance_inquiry()
+
+
+            elif brr == 's' or brr == 'S':
+                Bank.remittance()
+                print("")
+                Bank.balance_inquiry()
+                
+
+            elif brr == 'q' or brr == 'Q':
+                
+                frr = input("종료: q / 로그아웃: l  --> ")
+
+                if frr == 'l' or brr == 'L':
+                    print("")
+                    print("")
+                    
+                    Bank.account()
+                    
+                    
+                elif brr == 'q' or brr == 'Q':
+                    print("종료합니다.")
+                
+                break
+
+            else:
+                pass
+
+
+                
+        
+
+    def balance_inquiry(self):  # 잔액조회
+        
+        print("현재 잔액:", self.balance)
+
+    def desposit(self):  # 입금
+        arr = int(input ("입금할 금액:"))
+        self.balance += arr
+        print("==========================")
+        print("")
+        print("")
+        print(f"{arr}원이 입금되었습니다.")
+        
+
+    def withdraw(self): # 출금
+        drr = int(input ("출금할 금액:"))
+        grr = str(input("비밀번호를 입력해주세요: "))
+        if grr == "1234":
+            self.balance -= drr
+            print("==========================")
+            print("")
+            print("")
+            print(f"{drr}원이 출금되었습니다.")
+        
+        else: 
+            hrr = str(input("비밀번호가 틀렸습니다.  다시입력: r  --> / 돌아가기: h   --> "))
+            if hrr == 'r' or hrr == 'R':
+                Bank.withdraw()
+            elif hrr == 'h' or hrr == 'H':
+                Bank.order()
+            else: 
+                pass
+
+    def remittance(self):  # 송금
+        irr = input("송금할 계좌를 입력해주세요:  ")
+        jrr = int(input("송금할 금액을 입력해주세요:  "))
+
+        grr = str(input("비밀번호를 입력해주세요: "))
+        if grr == "1234":
+            self.balance -= jrr
+            print("==========================")
+            print("")
+            print("")
+            print(f"[{irr}]님에게 {jrr}원이 송금되었습니다.")
+        
+        else: 
+            hrr = str(input("비밀번호가 틀렸습니다.  다시입력: r / 돌아가기: h   --> "))
+            if hrr == 'r' or hrr == 'R':
+                Bank.withdraw()
+            elif hrr == 'h' or hrr == 'H':
+                Bank.order()
+            else: 
+                pass
+
+        
+
+
+Bank = Bank()
+Bank.account()
