061d569c3d453d356274e7ac95218729c3c2f9ed
LEESNAGWOO <33356618+gomtings@users.noreply.github.com>
2023-06-15 22:07:47 +0900

    class9 update
    파일 입출력 및 파일 및 폴더 삭제

---
+++ 수업예제/class9/filechk.py
@@ -0,0 +1,7 @@
+import os
+print(os.path.abspath('C:/GitHub/JeoninHighSchool/수업예제/class9/t1.txt'))
+print(os.path.isabs('C:/GitHub/JeoninHighSchool/수업예제/class9/t1.txt'))
+print(os.path.relpath('C:/GitHub/JeoninHighSchool/수업예제/class9/t1.txt'),'수업예제')
+print(os.path.basename('C:/GitHub/JeoninHighSchool/수업예제/class9/t1.txt'))
+print(os.path.dirname('C:/GitHub/JeoninHighSchool/수업예제/class9/t1.txt'))
+print(os.path.split('C:/GitHub/JeoninHighSchool/수업예제/class9/t1.txt'))
---
+++ 수업예제/class9/remove.py
@@ -0,0 +1,3 @@
+import os
+file_path = 'C:/GitHub/JeoninHighSchool/수업예제/class9/t1.txt'
+os.remove(file_path)
