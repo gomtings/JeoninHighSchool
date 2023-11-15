36d199cffd99c9d559e734e834c235d05627f1d5
leesangwoo <tkddn4508@gmail.com>
2023-11-16 01:25:42 +0900

    test001
    계속 -1 로만 들어옴...??

--- 아두이노_SW_EX/mega_2560test/mega_2560test.ino
+++ 아두이노_SW_EX/mega_2560test/mega_2560test.ino
@@ -1,34 +1,26 @@
 #include <SoftwareSerial.h>
-SoftwareSerial serial2(11,12); //15번을 RX, 14번을 TX, 처럼 사용할 수 있게 핀지정
+SoftwareSerial serial2(12,11); //15번을 RX, 14번을 TX, 처럼 사용할 수 있게 핀지정
 int echo =9;
 int trig = 10;
-bool flag = false; 
-int count = 0;
-String str = "";
+bool start = false;
 void setup() {
-  Serial.begin(9600);    //PC와 통신할 하드웨어 시리얼 시작
   serial2.begin(9600);  //아두이노와 통신할 소프트웨어 시리얼 시작
+  Serial.begin(9600);  //PC와 통신할 하드웨어 시리얼 시작
   pinMode(trig,OUTPUT);
   pinMode(echo,INPUT);
-}
-
-void loop() {
   digitalWrite(trig,LOW);
   digitalWrite(echo,LOW);
+}
+void loop() {
   delay(1);
   digitalWrite(trig,HIGH);
   delay(1);
   digitalWrite(trig,LOW);
   unsigned long duration = pulseIn(echo,HIGH);
-  float distance =  duration / 29.0/2.0;
-  str = String(distance);
-  if (serial2.available()||Serial.available()) {//시리얼 모니터에서 들어온 데이터가 있다면
-    flag = true;
+  double distance =  (duration/29.0)/2.0;
+  int ch = serial2.read();
+  if(ch >= -1){
+    serial2.print(distance);
   }
-  if(flag){
-        serial2.println(str);//시리얼 모니터에 표기
-        Serial.println(str);
-   }
   delay(10);
 }
-//[출처] 아두이노 시리얼 통신(UART) 무작정해보기|작성자 GONGZIPSA
