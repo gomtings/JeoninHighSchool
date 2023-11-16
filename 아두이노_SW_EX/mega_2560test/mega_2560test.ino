c9db7d3bdf442a1f006dff61c241daa30798bf04
LEESNAGWOO <33356618+gomtings@users.noreply.github.com>
2023-11-16 23:43:45 +0900

    시리얼 통신 복구...
    1.  메인 컨트롤러(우노) 에서 'g' 라는 메시지를 보내면 서브컨트롤러(메가) 에서 값을 확인하고 센서 데이터를 보내도록함.
    2. println 로 전송 함 받는 쪽에서 문자열 -> 실수 로 변환 할때 \n 확인해야 정확한 값 전달이 가능함.
    
    3. 추가 센서 연결시 구조 개선 필요.

--- 아두이노_SW_EX/mega_2560test/mega_2560test.ino
+++ 아두이노_SW_EX/mega_2560test/mega_2560test.ino
@@ -1,8 +1,9 @@
 #include <SoftwareSerial.h>
-SoftwareSerial serial2(12,11); //15번을 RX, 14번을 TX, 처럼 사용할 수 있게 핀지정
+SoftwareSerial serial2(11,12); //15번을 RX, 14번을 TX, 처럼 사용할 수 있게 핀지정
 int echo =9;
 int trig = 10;
-bool start = false;
+unsigned long duration = 0;
+double distance= 0;
 void setup() {
   serial2.begin(9600);  //아두이노와 통신할 소프트웨어 시리얼 시작
   Serial.begin(9600);  //PC와 통신할 하드웨어 시리얼 시작
@@ -11,16 +12,20 @@ void setup() {
   digitalWrite(trig,LOW);
   digitalWrite(echo,LOW);
 }
+//write() 함수가 바이트 또는 바이트의 시퀀스를 그대로 보내는 반면, print() 함수는 데이터를 사람이 읽을 수 있는 ASCII 텍스트로 변환하여 보낸다
 void loop() {
   delay(1);
   digitalWrite(trig,HIGH);
   delay(1);
   digitalWrite(trig,LOW);
-  unsigned long duration = pulseIn(echo,HIGH);
-  double distance =  (duration/29.0)/2.0;
-  int ch = serial2.read();
-  if(ch >= -1){
-    serial2.print(distance);
+  duration = pulseIn(echo,HIGH);
+  distance =  (duration/29.0)/2.0;
+  if (serial2.available()) {  // 소프트웨어 시리얼 포트에 데이터가 있는지 확인
+    char ch = serial2.read();  // 데이터를 읽음
+    if (ch == 'g') {  // 읽은 데이터가 'g'인지 확인 맞다면 센서 데이터를 전송한다.
+      serial2.println(distance);
+      Serial.println(distance);
+    }
   }
   delay(10);
 }
--- 아두이노_SW_EX/test_lrd/test_lrd.ino
+++ 아두이노_SW_EX/test_lrd/test_lrd.ino
@@ -4,9 +4,13 @@ void setup() {
   serial2.begin(9600);  //아두이노와 통신할 소프트웨어 시리얼 시작
   Serial.begin(9600);  //PC와 통신할 하드웨어 시리얼 시작
 }
+//write() 함수가 바이트 또는 바이트의 시퀀스를 그대로 보내는 반면, print() 함수는 데이터를 사람이 읽을 수 있는 ASCII 텍스트로 변환하여 보낸다
 void loop() {
-    serial2.write(57);
-    int ch = serial2.read();
-    Serial.print(ch);
-   delay(10);
+  serial2.write('g'); // 'g' 라는 문자를 보내서 상대측에게 데이터 요청.
+  if (serial2.available()) {  // 소프트웨어 시리얼 포트에 데이터가 있는지 확인
+    float distance = serial2.parseFloat();  // 부동 소수점 숫자를 읽음
+    Serial.print("Received distance: ");
+    Serial.println(distance);
+  }
+  delay(10);
 }
