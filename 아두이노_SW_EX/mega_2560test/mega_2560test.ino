cfcae5f348905609d174ecadea600c51d67d66b8
leesangwoo <tkddn4508@gmail.com>
2023-11-21 00:13:25 +0900

    update Timer and json
    타이머를 구조 추가 및 json 타입 추가.

--- 아두이노_SW_EX/mega_2560test/mega_2560test.ino
+++ 아두이노_SW_EX/mega_2560test/mega_2560test.ino
@@ -1,9 +1,32 @@
+#include <ArduinoJson.h> //ArduinoJson 라이브러리
+#include <TimerOne.h> // TimerOne 라이브러리
 #include <SoftwareSerial.h>
 SoftwareSerial serial2(11,12); //15번을 RX, 14번을 TX, 처럼 사용할 수 있게 핀지정
+
+#define SANSING_TIME 100
+int32_t sansingTime = 0;
+bool IssansingTime = false;
+#define SAND_TIME 100
+int32_t sandTime = 0;
+bool IssandeTime = false;
+
 int echo =9;
 int trig = 10;
 unsigned long duration = 0;
 double distance= 0;
+void mainTimer(void);
+void mainTimer(void){
+  if(IssansingTime == false)
+  {
+    sansingTime++;
+    if(sansingTime >= SANSING_TIME) IssansingTime = true;
+  }
+  if(IssandeTime == false)
+  {
+    sandTime++;
+    if(sandTime >= SAND_TIME) IssandeTime = true;
+  }
+}
 void setup() {
   serial2.begin(9600);  //아두이노와 통신할 소프트웨어 시리얼 시작
   Serial.begin(9600);  //PC와 통신할 하드웨어 시리얼 시작
@@ -11,20 +34,43 @@ void setup() {
   pinMode(echo,INPUT);
   digitalWrite(trig,LOW);
   digitalWrite(echo,LOW);
+  Timer1.initialize(1000); // 1ms마다 인터럽트 발생
+  Timer1.attachInterrupt(mainTimer); // 인터럽트 함수 지정
 }
 //write() 함수가 바이트 또는 바이트의 시퀀스를 그대로 보내는 반면, print() 함수는 데이터를 사람이 읽을 수 있는 ASCII 텍스트로 변환하여 보낸다
 void loop() {
+ if(IssansingTime == true){
+    ultrasonic();
+    sansingTime = 0;
+    IssansingTimeTime = false;
+ }else if(IssandeTime == true){
+    sandjson();
+    sandTime = 0;
+    IssandeTime = false;
+ }
+  ReadData();
+}
+
+void ultrasonic(){
   delay(1);
   digitalWrite(trig,HIGH);
   delay(1);
   digitalWrite(trig,LOW);
   duration = pulseIn(echo,HIGH);
   distance =  (duration/29.0)/2.0;
+}
+void sandjson(){
+      StaticJsonDocument<200> doc;
+      doc["distance"] = distance;
+      doc["Lidar"] = 0;
+      serializeJson(doc, serial2);
+      serial2.println();
+}
+void ReadData(){
   if (serial2.available()) {  // 소프트웨어 시리얼 포트에 데이터가 있는지 확인
     char ch = serial2.read();  // 데이터를 읽음
     if (ch == 'g') {  // 읽은 데이터가 'g'인지 확인 맞다면 센서 데이터를 전송한다.
-      serial2.println(distance);
-      Serial.println(distance);
+      sandjson()
     }
   }
   delay(10);
--- 아두이노_SW_EX/test_lrd/test_lrd.ino
+++ 아두이노_SW_EX/test_lrd/test_lrd.ino
@@ -1,11 +1,40 @@
+#include <ArduinoJson.h> //ArduinoJson 라이브러리
+#include <TimerOne.h> // TimerOne 라이브러리
 #include <SoftwareSerial.h>
 SoftwareSerial serial2(5, 6);  // 5번을 RX, 6번을 TX 처럼 사용할 수 있게 핀지정
+
+#define SAND_TIME 100
+int32_t sandTime = 0;
+bool IssandeTime = false;
+
+void mainTimer(void);
+void mainTimer(void){
+  if(IssansingTime == false)
+  {
+    sansingTime++;
+    if(sansingTime >= SANSING_TIME) IssansingTime = true;
+  }
+  if(IssandeTime == false)
+  {
+    sandTime++;
+    if(sandTime >= SAND_TIME) IssandeTime = true;
+  }
+}
 void setup() {
   serial2.begin(9600);  //아두이노와 통신할 소프트웨어 시리얼 시작
   Serial.begin(9600);  //PC와 통신할 하드웨어 시리얼 시작
+  Timer1.initialize(1000); // 1ms마다 인터럽트 발생
+  Timer1.attachInterrupt(mainTimer); // 인터럽트 함수 지정
 }
 //write() 함수가 바이트 또는 바이트의 시퀀스를 그대로 보내는 반면, print() 함수는 데이터를 사람이 읽을 수 있는 ASCII 텍스트로 변환하여 보낸다
 void loop() {
+  if(IssandeTime == true){
+    sandjson();
+    sandTime = 0;
+    IssandeTime = false;
+  }
+}
+void ReadData(){
   serial2.write('g'); // 'g' 라는 문자를 보내서 상대측에게 데이터 요청.
   if (serial2.available()) {  // 소프트웨어 시리얼 포트에 데이터가 있는지 확인
     float distance = serial2.parseFloat();  // 부동 소수점 숫자를 읽음
@@ -13,4 +42,23 @@ void loop() {
     Serial.println(distance);
   }
   delay(10);
+}
+void parseJson() {
+  StaticJsonDocument<200> doc;
+  DeserializationError error = deserializeJson(doc, Serial2);
+
+  if (error) {
+    Serial2.println("Failed to read from serial port");
+    return;
+  }
+
+  if (doc.containsKey("distance")) {
+    int distance = doc["distance"];
+    Serial2.println(distance);
+  }
+
+  if (doc.containsKey("Lidar")) {
+    int Lidar = doc["Lidar"];
+    Serial2.println(Lidar);
+  }
 }
