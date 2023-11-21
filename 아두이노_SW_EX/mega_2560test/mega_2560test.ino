e5aa862ec66592468b553acd649dc3a436f750f3
leesangwoo <tkddn4508@gmail.com>
2023-11-22 00:08:08 +0900

    update 1122

--- 아두이노_SW_EX/mega_2560test/mega_2560test.ino
+++ 아두이노_SW_EX/mega_2560test/mega_2560test.ino
@@ -1,14 +1,21 @@
 #include <ArduinoJson.h> //ArduinoJson 라이브러리
 #include <TimerOne.h> // TimerOne 라이브러리
 #include <SoftwareSerial.h>
+#include <DFRobot_TFmini.h>
 SoftwareSerial serial2(11,12); //15번을 RX, 14번을 TX, 처럼 사용할 수 있게 핀지정
+SoftwareSerial serial3(50,51);
 
+DFRobot_TFmini  TFmini;
+double space,strength;
 #define SANSING_TIME 100
 int32_t sansingTime = 0;
-bool IssansingTime = false;
-#define SAND_TIME 100
-int32_t sandTime = 0;
-bool IssandeTime = false;
+bool IssansingTime = false; // 초음파
+#define READ_TIME 10
+int32_t readTime = 0;
+bool IsreadTime = false; // 데이터 읽기
+#define RADARING_TIME 200
+int32_t radaringTime = 0;
+bool IsradaringTime = false; // 라이다
 
 int echo =9;
 int trig = 10;
@@ -16,20 +23,26 @@ unsigned long duration = 0;
 double distance= 0;
 void mainTimer(void);
 void mainTimer(void){
-  if(IssansingTime == false)
+  if(IsradaringTime == false) // 라이다 
+  {
+    radaringTime++;
+    if(radaringTime >= RADARING_TIME) IsradaringTime = true;
+  }
+  if(IssansingTime == false) // 초음파
   {
     sansingTime++;
     if(sansingTime >= SANSING_TIME) IssansingTime = true;
   }
-  if(IssandeTime == false)
+  if(IsreadTime == false) // 데이터읽기
   {
-    sandTime++;
-    if(sandTime >= SAND_TIME) IssandeTime = true;
+    readTime++;
+    if(readTime >= READ_TIME) IsreadTime = true;
   }
 }
 void setup() {
-  serial2.begin(9600);  //아두이노와 통신할 소프트웨어 시리얼 시작
   Serial.begin(9600);  //PC와 통신할 하드웨어 시리얼 시작
+  serial2.begin(9600);  //아두이노와 통신할 소프트웨어 시리얼 시작
+  TFmini.begin(serial3); // 라이다 센서와 통신할 하드웨어 시리얼 시작
   pinMode(trig,OUTPUT);
   pinMode(echo,INPUT);
   digitalWrite(trig,LOW);
@@ -42,13 +55,16 @@ void loop() {
  if(IssansingTime == true){
     ultrasonic();
     sansingTime = 0;
-    IssansingTimeTime = false;
- }else if(IssandeTime == true){
-    sandjson();
-    sandTime = 0;
-    IssandeTime = false;
+    IssansingTime = false;
+ }else if(IsradaringTime == true){
+    GetRader();
+    radaringTime = 0;
+    IsradaringTime == false;
+ }else if(IsreadTime == true){
+    ReadData();
+    readTime = 0;
+    IsreadTime == false;
  }
-  ReadData();
 }
 
 void ultrasonic(){
@@ -62,16 +78,22 @@ void ultrasonic(){
 void sandjson(){
       StaticJsonDocument<200> doc;
       doc["distance"] = distance;
-      doc["Lidar"] = 0;
+      doc["Lidar"] = space;
       serializeJson(doc, serial2);
       serial2.println();
 }
+void GetRader(){
+  if (TFmini.measure()) {// 거리와 신호의 강도를 측정합니다. 성공하면 을 반환하여 if문이 작동합니다.
+    space = TFmini.getDistance();// 거리값을 cm단위로 불러옵니다.
+    strength = TFmini.getStrength();// 신호의 강도를 불러옵니다. 측정 대상이 넓으면 강도가 커집니다.
+  }
+}
 void ReadData(){
-  if (serial2.available()) {  // 소프트웨어 시리얼 포트에 데이터가 있는지 확인
+  if (serial2.available()) {  // 소프트웨어 시리얼 포트에 데이터가 있는지 확인 serial2.available()
     char ch = serial2.read();  // 데이터를 읽음
-    if (ch == 'g') {  // 읽은 데이터가 'g'인지 확인 맞다면 센서 데이터를 전송한다.
-      sandjson()
+    Serial.print(ch);
+    if (ch == 'g') { // 읽은 데이터가 'g'인지 확인 맞다면 센서 데이터를 전송한다.
+      sandjson();
     }
   }
-  delay(10);
 }
--- 아두이노_SW_EX/test_lrd/test_lrd.ino
+++ 아두이노_SW_EX/test_lrd/test_lrd.ino
@@ -2,18 +2,16 @@
 #include <TimerOne.h> // TimerOne 라이브러리
 #include <SoftwareSerial.h>
 SoftwareSerial serial2(5, 6);  // 5번을 RX, 6번을 TX 처럼 사용할 수 있게 핀지정
+uint16_t space,strength;
 
 #define SAND_TIME 100
 int32_t sandTime = 0;
 bool IssandeTime = false;
 
+double distance = 0.00;
+double Lidar = 0.00;
 void mainTimer(void);
 void mainTimer(void){
-  if(IssansingTime == false)
-  {
-    sansingTime++;
-    if(sansingTime >= SANSING_TIME) IssansingTime = true;
-  }
   if(IssandeTime == false)
   {
     sandTime++;
@@ -29,7 +27,7 @@ void setup() {
 //write() 함수가 바이트 또는 바이트의 시퀀스를 그대로 보내는 반면, print() 함수는 데이터를 사람이 읽을 수 있는 ASCII 텍스트로 변환하여 보낸다
 void loop() {
   if(IssandeTime == true){
-    sandjson();
+    ReadData();
     sandTime = 0;
     IssandeTime = false;
   }
@@ -37,28 +35,18 @@ void loop() {
 void ReadData(){
   serial2.write('g'); // 'g' 라는 문자를 보내서 상대측에게 데이터 요청.
   if (serial2.available()) {  // 소프트웨어 시리얼 포트에 데이터가 있는지 확인
-    float distance = serial2.parseFloat();  // 부동 소수점 숫자를 읽음
-    Serial.print("Received distance: ");
+    StaticJsonDocument<200> doc;
+    DeserializationError error = deserializeJson(doc, serial2);
+
+    if (error) {
+      Serial.println("Failed to read from serial port");
+      IssandeTime = false;
+      return;
+    }
+    
+    distance = doc["distance"];
+    Lidar = doc["Lidar"];
     Serial.println(distance);
-  }
-  delay(10);
-}
-void parseJson() {
-  StaticJsonDocument<200> doc;
-  DeserializationError error = deserializeJson(doc, Serial2);
-
-  if (error) {
-    Serial2.println("Failed to read from serial port");
-    return;
-  }
-
-  if (doc.containsKey("distance")) {
-    int distance = doc["distance"];
-    Serial2.println(distance);
-  }
-
-  if (doc.containsKey("Lidar")) {
-    int Lidar = doc["Lidar"];
-    Serial2.println(Lidar);
+    Serial.println(Lidar);
   }
 }
