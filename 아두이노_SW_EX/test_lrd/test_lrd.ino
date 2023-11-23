185ec2db01b11c5a0390faa6de82973e4a3bef04
LEESNAGWOO <33356618+gomtings@users.noreply.github.com>
2023-11-23 21:38:00 +0900

    update 1123 - 장치간 통신 해결
    1. 아두이노간 통신문제 해결. 센서보드에서 HW 시리얼로 변경.
    2. 모터 컨트롤 보드에서 원활한 통신을 위해 1초간 대기 시간 추가.
    3. 센서보드 에서 위 수정사항에 맞춘 포트 변경

--- 아두이노_SW_EX/test_lrd/test_lrd.ino
+++ 아두이노_SW_EX/test_lrd/test_lrd.ino
@@ -35,6 +35,38 @@ void loop() {
 }
 void ReadData(){
   serial2.listen(); //
+  delay(1); // listen 함수 호출 후에 약간의 지연을 추가합니다.
+  serial2.write('g'); // 'g' 라는 문자를 보내서 상대측에게 데이터 요청.
+
+  unsigned long startTime = millis(); // 현재 시간을 저장합니다.
+  while(!serial2.available()) {  // 소프트웨어 시리얼 포트에 데이터가 있는지 확인
+    if (millis() - startTime > 1000) { // 1초 동안 데이터가 도착하지 않으면 루프를 종료 오류가 발생할 경우 시간을 늘려야 함. (아두이노간 통신 타이밍 문제 해결)
+      Serial.println("Timeout waiting for data");
+      return;
+    }
+  }
+
+  StaticJsonDocument<256> doc;
+  DeserializationError error = deserializeJson(doc, serial2);
+  if (error) {
+    Serial.println("Failed to read from serial port");
+    IssandeTime = false;
+    return;
+  }
+  distance = doc["distance"];
+  Lidar = doc["Lidar"];
+  x = doc["Accel_x"];
+  y = doc["Accel_y"];
+  z = doc["Accel_z"];
+  Serial.println(distance);
+  Serial.println(Lidar);
+  Serial.println(x);
+  Serial.println(y);
+  Serial.println(z);
+}
+/*
+void ReadData(){
+  serial2.listen(); //
   serial2.write('g'); // 'g' 라는 문자를 보내서 상대측에게 데이터 요청.
   if (serial2.available()) {  // 소프트웨어 시리얼 포트에 데이터가 있는지 확인
     StaticJsonDocument<256> doc;
@@ -44,7 +76,6 @@ void ReadData(){
       IssandeTime = false;
       return;
     }
-    
     distance = doc["distance"];
     Lidar = doc["Lidar"];
     x = doc["Accel_x"];
@@ -57,3 +88,4 @@ void ReadData(){
     Serial.println(z);
   }
 }
+*/
