a99387d892ac0242a7a5fc765cf6dea7936e8668
LEESNAGWOO <33356618+gomtings@users.noreply.github.com>
2024-08-30 08:58:12 +0900

    기초 02

---
+++ 수업예제/24-1방과후수업(python)/2학기/ex01.py
@@ -0,0 +1,17 @@
+# 정수
+x = 42
+y = 10
+print(x,type(x))
+# 실수
+x = 3.14
+y = 1.0
+print(x,type(x))
+# 복소수
+z = 3 + 4j
+print(z,type(z))
+
+#문자열
+name = "안녕하세요"
+msg = 'hello world'
+print(name,type(name))
+print(msg,type(msg))
