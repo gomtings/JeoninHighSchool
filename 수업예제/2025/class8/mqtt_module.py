ce3360db929f605c0fad1de2a40deb68162baa31
sangwoo <tkddn4508@naver.com>
2025-07-25 21:23:48 +0900

    fix

--- 수업예제/2025/class8/mqtt_module.py
+++ 수업예제/2025/class8/mqtt_module.py
@@ -1,6 +1,5 @@
 import paho.mqtt.client as mqtt
 import json
-import queue
 import uuid
 class MQTTClient:
     def __init__(self,islocal = False):
@@ -19,11 +18,11 @@ class MQTTClient:
         return self.Mqtt_Connection
 
     def update_friend(self,friend):
+        print(friend)
         self.friend = friend
         
     def on_connect(self,client, userdata, flags, rc):
         if rc == 0:
-            print("on_connect")
             self.Mqtt_Connection = True
         else:
             self.Mqtt_Connection = False
@@ -33,19 +32,31 @@ class MQTTClient:
         pass
     
     def on_publish(self,client, userdata, mid):
-        print("In on_pub callback mid= ", mid)
+        #print("In on_pub callback mid= ", mid)
+        pass
         
     def on_subscribe(self,client, userdata, mid, granted_qos):
         #print("subscribed: " + str(mid) + " " + str(granted_qos))
         pass    
     
-    def on_message(self,client,userdata,msg):
+    def on_message(self, client, userdata, msg):
         str_msg = msg.payload.decode("utf-8")
-        if msg.topic == "Event/State/" :
-            self.State = json.loads(str_msg)
-        for friend in self.friend:
-            if msg.topic == f"Event/Chat/{friend}" :
-                self.State = json.loads(str_msg)
+        # topic 확인
+        if msg.topic == "Event/Chat/State/":
+            try:
+                self.State = str_msg
+            except ValueError as e:
+                print(f"JSON 파싱 오류 (State): {e}")
+                return
+        
+        if isinstance(self.friend, list):
+            for friend in self.friend:
+                if msg.topic == f"Event/Chat/{friend}":
+                    try:
+                        self.State = json.loads(str_msg)
+                    except ValueError as e:
+                        print(f"JSON 파싱 오류 (Friend): {e}")
+                        return
             
     def msg(self,topics,message):
         self.client.publish(topics,message, 1) #Event/T-MDS/YJSensing/
@@ -62,17 +73,8 @@ class MQTTClient:
         self.client.on_publish = self.on_publish
         self.client.on_subscribe = self.on_subscribe
         self.client.on_message = self.on_message
-        self.client.username_pw_set('ari_mqtt', "25846ec82")
-        self.client.connect('211.169.215.170', 53200, keepalive=120)
-    
-    def localconnect(self):
-        self.client.on_connect = self.on_connect
-        self.client.on_disconnect = self.on_disconnect
-        self.client.on_publish = self.on_publish
-        self.client.on_subscribe = self.on_subscribe
-        self.client.on_message = self.on_message
-        self.client.username_pw_set('admin', "solimatics")
-        self.client.connect('Solimatics.iptime.org', 53200, keepalive=60)
+        self.client.username_pw_set('sol_mqtt', "solimatics")
+        self.client.connect('211.188.48.74', 53200, keepalive=120)
         
     def loop_start(self):
         self.client.loop_start()
